84386bfda4c61b4ca011f812a662389fa75cf8d8
José David Montoya <78799535+jdmg6-ua@users.noreply.github.com>
2023-01-28 21:12:45 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://jdmg6-ua.github.io",
-  base: "/blog"
+  base: "/astroBlog"
 });
